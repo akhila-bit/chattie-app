5fce8b6cd6bca528aea3eef0ac3f963943078a85
Sreenath Thalla <akhila@automate.io>
2023-09-14 23:26:20 +0530

    initialised fiebase poject

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDCbNZTSaZkDq5gFHXU0pfxRa_jyrB0gpQ',
+  authDomain: 'chattie12.firebaseapp.com',
+  databaseURL:
+    'https://chattie12-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'chattie12',
+  storageBucket: 'chattie12.appspot.com',
+  messagingSenderId: '941746988130',
+  appId: '1:941746988130:web:7d9c315865c4ef6a729d3b',
+};
+const app = firebase.initializeApp(config);
